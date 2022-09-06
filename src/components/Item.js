f6e3a7e245a6ae2a47a3a0fb39c61f3ad72e76c8
ChaseWelsh <fseusb@gmail.com>
2022-09-06 10:49:18 -0600

    commit tree ca95d45a0f5fa705bd1985406d82a7e6afe11e2b

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,14 +1,33 @@
 import React from "react";
 
-function Item({ item }) {
+function Item({ item, onUpdateItem, onDeleteItem }) {
+
+  function toggleCart(event) {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method:"PATCH",
+      headers: {
+        "Content-Type": "application/json"
+      },
+      body: JSON.stringify({
+        isInCart:!item.isInCart
+      })
+    }).then(r=>r.json()).then(data=>onUpdateItem(data))
+  }
+
+  function yeet() {
+    fetch(`http://localhost:4000/items/${item.id}`, {
+      method: "DELETE"
+    }).then(r=>r.json()).then((()=>{onDeleteItem(item)}))
+  }
+
   return (
     <li className={item.isInCart ? "in-cart" : ""}>
       <span>{item.name}</span>
       <span className="category">{item.category}</span>
-      <button className={item.isInCart ? "remove" : "add"}>
+      <button className={item.isInCart ? "remove" : "add"} onClick={toggleCart}>
         {item.isInCart ? "Remove From" : "Add to"} Cart
       </button>
-      <button className="remove">Delete</button>
+      <button className="remove" onClick={yeet}>Delete</button>
     </li>
   );
 }
